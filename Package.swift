b3ee616ede3d60f73e73890d8cfe19be482e6e0e
Bradley David Bergeron <brad@bradbergeron.com>
2023-09-22 13:47:33 -0400

    Bump platform versions

--- Package.swift
+++ Package.swift
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
   name: "Stubby",
   platforms: [
-    .macOS(.v10_13),
-    .iOS(.v12),
+    .macOS(.v10_15),
+    .iOS(.v13),
   ],
   products: [
     .library(name: "Stubby", targets: ["Stubby"]),
