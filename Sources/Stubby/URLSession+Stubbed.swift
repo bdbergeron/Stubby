e7b755d56d3103250b460b403c1d55e67d7513db
Bradley David Bergeron <brad@bradbergeron.com>
2023-10-12 17:58:01 -0400

    Add more APIs

--- Sources/Stubby/URLSession+Stubbed.swift
+++ Sources/Stubby/URLSession+Stubbed.swift
@@ -4,12 +4,12 @@ import Foundation
 
 extension URLSession {
   /// Create a `URLSession` with stubbed request handlers.
-  /// - Parameter responseProvider: The type of ``StubbyResponseProvider`` to use for handling requests.
-  /// - Parameter configuration: An `URLSessionConfiguration` object to be used by the created `URLSession`.
-  ///   Defaults to `URLSessionConfiguration.ephemeral`.
-  /// - Parameter maintainExistingProtocolClasses: By default, the created `URLSession` will utilize _only_ the underlying
-  ///   `StubbyURLProtocol` protocol class. To maintain any existing protocol classes specified by `URLSessionConfiguration.protocolClasses`,
-  ///   set this to `true`.
+  /// - Parameters:
+  ///   - responseProvider: The type of ``StubbyResponseProvider`` to use for handling requests.
+  ///   - configuration: An `URLSessionConfiguration` object to be used by the created `URLSession`.
+  ///     Defaults to `URLSessionConfiguration.ephemeral`.
+  ///   - maintainExistingProtocolClasses: By default, the created `URLSession` will utilize _only_ the underlying `StubbyURLProtocol`
+  ///     protocol class. To maintain any existing protocol classes specified by `URLSessionConfiguration.protocolClasses`, set this to `true`.
   /// - Returns: A new ``URLSession`` instance.
   public static func stubbed<ResponseProvider: StubbyResponseProvider>(
     responseProvider: ResponseProvider.Type,
@@ -29,4 +29,102 @@ extension URLSession {
     }
     return URLSession(configuration: configuration)
   }
+  
+  /// Create a `URLSession` with stubbed request handlers.
+  /// - Parameters:
+  ///   - configuration: An `URLSessionConfiguration` object to be used by the created `URLSession`.
+  ///     Defaults to `URLSessionConfiguration.ephemeral`.
+  ///   - maintainExistingProtocolClasses: By default, the created `URLSession` will utilize _only_ the underlying `StubbyURLProtocol`
+  ///     protocol class. To maintain any existing protocol classes specified by `URLSessionConfiguration.protocolClasses`, set this to `true`.
+  ///   - stubs: A list of ``Stub``s to use.
+  /// - Returns: A new ``URLSession`` instance.
+  public static func stubbed(
+    configuration: URLSessionConfiguration = .ephemeral,
+    maintainExistingProtocolClasses: Bool = false,
+    _ stubs: [Stub])
+    -> URLSession
+  {
+    for stub in stubs {
+      ResponseProvider.registerStub(stub)
+    }
+    return stubbed(
+      responseProvider: ResponseProvider.self,
+      configuration: configuration,
+      maintainExistingProtocolClasses: maintainExistingProtocolClasses)
+  }
+  
+  /// Create a `URLSession` with stubbed request handlers.
+  /// - Parameters:
+  ///   - configuration: An `URLSessionConfiguration` object to be used by the created `URLSession`.
+  ///     Defaults to `URLSessionConfiguration.ephemeral`.
+  ///   - maintainExistingProtocolClasses: By default, the created `URLSession` will utilize _only_ the underlying `StubbyURLProtocol`
+  ///     protocol class. To maintain any existing protocol classes specified by `URLSessionConfiguration.protocolClasses`, set this to `true`.
+  ///   - url: ``URL`` to stub.
+  ///   - response: Response to stub.
+  /// - Returns: A new ``URLSession`` instance.
+  public static func stubbed(
+    configuration: URLSessionConfiguration = .ephemeral,
+    maintainExistingProtocolClasses: Bool = false,
+    url: URL,
+    response: @escaping (URLRequest) throws -> Result<StubbyResponse, Error>)
+    -> URLSession
+  {
+    stubbed(
+      configuration: configuration,
+      maintainExistingProtocolClasses: maintainExistingProtocolClasses,
+      [
+        .init(url: url, response: response),
+      ])
+  }
+}
+
+// MARK: - Stub
+
+public struct Stub {
+
+  // MARK: Lifecycle
+
+  public init(
+    url: URL,
+    response: @escaping (URLRequest) throws -> Result<StubbyResponse, Error>)
+  {
+    self.url = url
+    self.response = response
+  }
+
+  // MARK: Public
+
+  public let url: URL
+  public let response: (URLRequest) throws -> Result<StubbyResponse, Error>
+
+}
+
+// MARK: - ResponseProvider
+
+private struct ResponseProvider: StubbyResponseProvider {
+
+  // MARK: Internal
+
+  static func registerStub(_ stub: Stub) {
+    stubs[stub.url] = stub
+  }
+
+  static func respondsTo(request: URLRequest) -> Bool {
+    true
+  }
+
+  static func response(for request: URLRequest) throws -> Result<StubbyResponse, Error> {
+    guard let url = request.url else {
+      throw URLError(.badURL)
+    }
+    guard let stub = stubs[url] else {
+      throw URLError(.unsupportedURL)
+    }
+    return try stub.response(request)
+  }
+
+  // MARK: Private
+
+  private static var stubs = [URL: Stub]()
+
 }
--- Tests/StubbyTests/StubbyTests.swift
+++ Tests/StubbyTests/StubbyTests.swift
@@ -19,7 +19,9 @@ final class StubbyTests: XCTestCase {
   }
 
   func test_stubbyResponse_failsWithUnsupportedURLError() async {
-    let urlSession = URLSession.stubbed(responseProvider: TestResponseProvider.self)
+    let urlSession = URLSession.stubbed(url: .githubURL) { _ in
+      .failure(URLError(.unsupportedURL))
+    }
     let request = URLRequest(url: .githubURL)
     do {
       _ = try await urlSession.data(for: request)
@@ -35,12 +37,57 @@ final class StubbyTests: XCTestCase {
   }
 
   func test_stubbyResponse_succeeds() async {
-    let urlSession = URLSession.stubbed(responseProvider: TestResponseProvider.self)
+    let urlSession = URLSession.stubbed(url: .repoURL) { request in
+      try .success(StubbyResponse(
+        data: XCTUnwrap("Hello, world!".data(using: .utf8)),
+        for: XCTUnwrap(request.url)))
+    }
     let request = URLRequest(url: .repoURL)
     do {
       let (data, _) = try await urlSession.data(for: request)
       let string = String(data: data, encoding: .utf8)
       XCTAssertEqual(string, "Hello, world!")
+    } catch {
+      XCTFail("Unexpected error: \(error)")
+    }
+  }
+
+  func test_stubbyResponse_multipleStubs() async throws {
+    let githubExpectation = expectation(description: "github")
+    let repoExpectation = expectation(description: "repo")
+    let urlSession = URLSession.stubbed([
+      Stub(url: .githubURL) { request in
+        defer { githubExpectation.fulfill() }
+        return try .success(StubbyResponse(
+          data: XCTUnwrap("Github".data(using: .utf8)),
+          for: XCTUnwrap(request.url)))
+      },
+      Stub(url: .repoURL) { request in
+        defer { repoExpectation.fulfill() }
+        return try .success(StubbyResponse(
+          data: XCTUnwrap("Hello, world!".data(using: .utf8)),
+          for: XCTUnwrap(request.url)))
+      },
+    ])
+    async let requests = [
+      urlSession.data(from: .githubURL),
+      urlSession.data(from: .repoURL),
+    ]
+    let responses = try await requests
+    await fulfillment(of: [githubExpectation, repoExpectation], timeout: 1.0)
+    XCTAssertEqual(responses.count, 2)
+    let (githubData, _) = responses[0]
+    let (repoData, _) = responses[1]
+    XCTAssertEqual(String(data: githubData, encoding: .utf8), "Github")
+    XCTAssertEqual(String(data: repoData, encoding: .utf8), "Hello, world!")
+    do {
+      _ = try await urlSession.data(from: URL(string: "https://bradbergeron.com")!)
+      XCTFail("Should fail.")
+    } catch let error as NSError {
+      XCTAssertEqual(error.domain, URLError.errorDomain)
+      let expectedError = URLError(.unsupportedURL)
+      XCTAssertEqual(error.code, expectedError.errorCode)
+      XCTAssertEqual(error.localizedDescription, expectedError.localizedDescription)
     } catch {
       XCTFail("Unexpected error: \(error)")
     }
