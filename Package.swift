dc7a8e9029bf76070fd976f9076d2275f439d6c9
Bradley David Bergeron <brad@bradbergeron.com>
2025-10-11 18:47:52 -0700

    Update packages

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@
 import PackageDescription
 
 let package = Package(
-  name: "Stubby",
-  platforms: [
-    .macOS(.v10_13),
-    .iOS(.v12),
-  ],
-  products: [
-    .library(name: "Stubby", targets: ["Stubby"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-docc-plugin", .upToNextMajor(from: "1.3.0")),
-  ],
-  targets: [
-    .target(
-      name: "Stubby",
-      dependencies: []),
-    .testTarget(
-      name: "StubbyTests",
-      dependencies: [
-        .target(name: "Stubby"),
-      ]),
-  ]
+    name: "Stubby",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+    ],
+    products: [
+        .library(name: "Stubby", targets: ["Stubby"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", .upToNextMajor(from: "1.4.5")),
+    ],
+    targets: [
+        .target(
+            name: "Stubby",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "StubbyTests",
+            dependencies: [
+                .target(name: "Stubby"),
+            ]
+        ),
+    ]
 )
